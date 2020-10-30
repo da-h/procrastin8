6ef150d423be72e8c238f0ff92ac672670e1fadb
David Hartmann <dhartmann@uni-mainz.de>
2020-10-30 07:48:43 +0100

    added radioline

--- ui.py
+++ ui.py
@@ -231,6 +231,48 @@ class HLine(UIElement):
                 self.printAt((0,1), self.text+" ")
 
 
+class RadioLine(UIElement):
+    def __init__(self, text, choices, wrapper, parent=None):
+        super().__init__(parent=parent)
+        self.height = 2
+        self.text = text
+        self.choices = choices
+        self.wrapper = wrapper
+        self.active = 0
+
+    def typeset(self):
+        pass
+
+    def draw(self, clean=False):
+        super().draw(clean)
+        # check what highlight it is
+        highlight = lambda x: term.ljust(x,width=self.wrapper.width)
+        if term.cursor.on_element == self:
+            highlight = lambda x: term.bold_green(term.ljust(x, width=self.wrapper.width))
+
+        self.printAt((0,0), highlight(self.text))
+        # self.printAt((3,1), highlight("".join("◢" + term.black_on_white(c) + "◤" if i == self.active else c for i, c in enumerate(self.choices))))
+
+        choice = [term.black_on_green(" "+c+" ") if i == self.active else " "+c+" " for i, c in enumerate(self.choices)]
+        choice[self.active] = term.green("◢") + choice[self.active] + term.green("◤")
+        choice_text = term.green(term.bold("/")).join(choice[:self.active]) + choice[self.active] + term.green(term.bold("/")).join(choice[self.active+1:])
+        if self.active != 0:
+            choice_text = " "+choice_text
+        if self.active != len(choice):
+            choice_text += " "
+        self.printAt((3,1), choice_text)
+
+    def cursorAction(self, val):
+        if val.code == term.KEY_LEFT:
+            self.active = (self.active - 1) % len(self.choices)
+            return
+        elif val.code == term.KEY_RIGHT:
+            self.active = (self.active + 1) % len(self.choices)
+            return
+        return super().cursorAction(val)
+
+
+
 
 class PlainWindow(UIElement):
 
@@ -347,7 +389,7 @@ class TaskWindow(TextWindow):
         self.manage(elem)
 
 
-class CenteredWindow(TextWindow):
+class Prompt(TextWindow):
 
     def __init__(self, width, parent=None):
         rel_pos = ((term.width - width)//2,10)
@@ -375,11 +417,12 @@ class Sidebar(TextWindow):
         super().__init__(rel_pos, width=width, title="Settings", parent=parent)
         self.draw_style = "basic-left-edge"
 
-        self.add_line("Add")
-        self.add_line("Remove")
-        self.add_line("Tag")
-        self.add_line("Lorem")
-        self.add_line("Ipsum")
+        self.lines.append(RadioLine("Verbosity",["Small","Medium","Full"], wrapper=self.wrapper, parent=self))
+        self.manage(self.lines[-1])
+        self.lines.append(RadioLine("Verbosity",["Small","Medium","Full"], wrapper=self.wrapper, parent=self))
+        self.manage(self.lines[-1])
+        self.lines.append(RadioLine("Verbosity",["Small","Medium","Full"], wrapper=self.wrapper, parent=self))
+        self.manage(self.lines[-1])
 
     def cursorAction(self, val):
 
@@ -474,7 +517,7 @@ class Dashboard(UIElement):
             elif val == "-":
                 self.rel_pos += (-4,-3)
             elif val == "s" and self.overlay is None:
-                # self.overlay = CenteredWindow(COLUMN_WIDTH, parent=self)
+                # self.overlay = Prompt(COLUMN_WIDTH, parent=self)
                 self.overlay = Sidebar(COLUMN_WIDTH, parent=self)
                 self.overlay.draw()
                 self.rel_pos = (COLUMN_WIDTH,0)
