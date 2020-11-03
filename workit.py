2edf88077321002a1aa29023d911ccb09be6e6e6
David Hartmann <dhartmann@uni-mainz.de>
2020-11-03 19:17:55 +0100

    changeable todo-file added

--- workit.py
+++ workit.py
@@ -1,6 +1,8 @@
 from ui import get_term
 from ui import Dashboard
 from model import Model
+import sys
+from os.path import dirname, join
 
 term = get_term()
 
@@ -9,6 +11,12 @@ def main():
     draw_calls = 0
 
     # init
+    if len(sys.argv) == 1:
+        todofile = "TODO.md"
+        donefile = "DONE.md"
+    else:
+        todofile = sys.argv[-1]
+        donefile = join(dirname(todofile), "done.md")
     model = Model("TODO.md", "DONE.md")
     dash = Dashboard(model)
 
